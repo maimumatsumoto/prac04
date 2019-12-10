c5a81e37f24ff3309d6ab3483f7f312ce3e578ad
maimumatsumoto <maimu.matsumoto@my.jcu.edu.au>
2019-12-11 01:21:15 +0800

    finish

---
+++ lists_warmup.py
@@ -0,0 +1,16 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+#//1. Change the first element of numbers to "ten"//#
+numbers[0]="ten"
+print(numbers)
+
+#//2. Change the last element of numbers to 1//#
+numbers[-1]=1
+print(numbers)
+
+
+#//3. Get all the elements from numbers except the first two//#
+print(numbers[2:])
+
+#//Check if 9 is an element of numbers//#
+print(9 in numbers)
